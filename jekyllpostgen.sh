f017bade3c435687bc52a9ca971f96c5ddf2b6ff
Eric Hoog <eric_hoog@hotmail.com>
2017-11-01 21:41:08 -0400

    remove unneeded function

--- jekyllpostgen.sh
+++ jekyllpostgen.sh
@@ -56,38 +56,6 @@ elif [ -z "$1" ]; then
   exit 0
 fi
 
-# Y/n ask function
-function ask {
-    while true; do
-
-        if [ "${2:-}" = "true" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "false" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the question
-        read -p "$1 [$prompt] " REPLY
-
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-
-    done
-}
-
 ##### Variables #####
 
 # post title
