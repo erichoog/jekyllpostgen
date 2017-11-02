e5ca290bc4eaf69f8712cf2f4b007cd655cd395d
Eric Hoog <eric_hoog@hotmail.com>
2017-11-01 21:30:43 -0400

    first draft

--- jekyllpostgen.sh
+++ jekyllpostgen.sh
@@ -9,30 +9,41 @@
 #-----------------------------#
 
 # Simply put the script in your site directory, edit the configs to fit your setup, and run it with:
-# ./post "post title"
+# ./jekyllpostgen.sh "post title"
 
 # What is does:
 # - creates the post with the correct format of date and title
 # - adds YAML front-matter (layout, title, date, tags)
 # - opens the post file in editor chosen
 
+# EXAMPLE OUTPUT:
+
+# ---
+# layout: post
+# title: "A Canvas of the Minds"
+# href: https://acanvasoftheminds.com/
+# date: 2017-10-27 22:57:09 -0400
+# tags: [wordpress, personal_stories, collaborative, blogs]
+# categories: EN
+# ---
 
 ########## Configs ##########
 
 # Post layout
-layout=post
+layout="post"
 
 # Post text editor
-editor=cat
+editor="cat"
 
 # Post directory
-folder=_posts/
+folder="_posts/"
 
-date_format='date +%F" "%T" "%z'
-date=$(date_format)
+# Default date formate in posts
+default_date_format_in_post='%F %T %z'
 
-# Author
-#author="Marcus Hammarberg"
+# Default category (space separated values)
+default_category="EN"
+
 
 ########## Program ##########
 
@@ -89,28 +100,34 @@ title="$1"
 filetitle=$( echo ${1// /-} | awk '{print tolower($0)}'| sed 's/[^a-z\-]*//g')
 
 # name of file
-filename="$folder`date +%F`-$filetitle.md"
+filename="${folder}`date +%F`-$filetitle.md"
 echo $filename
 
+date=$(date +"$default_date_format_in_post")
 
 ########## Adding to file ##########
 echo "---" >> $filename
-echo "layout: $layout" >> $filename
+echo "layout: ${layout}" >> $filename
 echo "title: \"$title\"" >> $filename
-#echo "author: \"$author\"" >> $filename
+
+### Adding href to file
+read -p "Href: " href
+if [ "$href" ]; then
+  echo "href: $href" >> $filename
+fi
+
 echo "date: $date" >> $filename
 
-
-### Adding tags on new lines with a dash in front (separated with comma)
+### Adding tags
 read -p "Tags: " tags
 if [ "$tags" ]; then
-  echo "tags:
-- $tags" | sed 's/,/\
--/g' >> $filename
+  echo "tags: [$tags]" >> $filename
 fi
+
+echo "categories: ${default_category}" >> $filename
 
 echo "---" >> $filename
 echo >> $filename
 
 # open in chosen editor
-$editor $filename
+${editor} $filename
