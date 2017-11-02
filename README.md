76726f0a44b9c99044ec8aa14bf78c3061f4cea9
Eric <Eric_hoog@hotmail.com>
2017-11-02 10:17:47 -0400

    Initial Implementation (#1)
    * add credit / inspiration references
    
    * add source script
    
    * initial work
    
    * first draft
    
    * remove unneeded function
    
    * allow digits in file name
    
    * add command line options and interactivity
    
    * create some functions and clean up code
    
    * update readme on expected usage
    
    * update code
    
    * fix line in readme
    
    * fix bug in set_filename usage
    
    * add note for tag and categories options
    
    * fix wording
    
    * add config for two different tag formats
    
    * update readme for tag formatting options

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # jekyllpostgen
 A Jekyll Post Generator for BASH
+
+Credit / Inspired by: 
+- [Jekyll Post Creator](http://www.marcusoft.net/2014/12/my-post-scaffolder-for-jekyll.html)
+- [jekyll-posts-generator](https://github.com/ismnoiet/jekyll-posts-generator)
+
+# Usage
+
+The filename will always output in this format: `YYYY-MM-DD-your-file-title.md`
+
+In the config function at the top of the script, the editor is set to `cat` by default so the file contents will be opened with that program at the end.
+
+## Basic:
+
+`./jekyllpostgen.sh "Your File Title"`
+
+This will create a file with this format:
+
+```
+---
+layout: post
+title: "Your File Title"
+---
+```
+
+## Interactive:
+
+`./jekyllpostgen.sh -i`
+
+This will read in Title, Href, Tags, and Categories one by one:
+```
+Enter Title: "Your File Title"
+Enter Href (link): https://test.com
+Enter Tags (comma separated): test1, test with space, test2
+Enter Categories (space separated): DE
+```
+
+This will create a file with this format:
+```
+---
+layout: post
+title: "Your File Title"
+href: https://test.com
+date: 2017-11-02 09:23:57 -0400
+tags: [test1, test with space, test2]
+categories: DE
+---
+```
+
+## Command-line Options:
+
+`./jekyllpostgen.sh "Your File Title" -t "tag1, tag with space, tag3" -l https://test.com -c DE`
+
+This will create a file with this format:
+```
+---
+layout: post
+title: "Your File Title"
+href: https://test.com
+date: 2017-11-02 09:30:07 -0400
+tags: [tag1, tag with space, tag3]
+categories: DE
+---
+```
+
+> NOTE: for -t and -c options you can use double quotes for multiple options or ommit them for one.
+
+# Configuration
+
+This is done in the config function block at the top of the script.
+```
+function config {
+
+}
+```
+
+## Tag Format Options
+
+The default tag format is the brackets format: `tags: [tag1, tag2, tag3]`
+
+This is set with the `tag_format_is_brackets=true` option in the config function.
+
+To change this to the hyphenated format: 
+
+```
+tags:
+- tag1
+- tag2
+- tag3
+```
+
+Set the `tag_format_is_brackets` option to `false`:  `tag_format_is_brackets=false`
---
+++ jekyllpostgen.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+#--------jekyllpostgen--------#
+#
+# Source and tweaked from:
+#   https://gist.github.com/marcusoftnet/2eec785d3477beacf709#file-scaffold_post
+#   https://gist.github.com/marcusoftnet/2eec785d3477beacf709#file-scaffold_post
+#
+#-----------------------------#
+
+# Simply put the script in your site directory, edit the configs to fit your setup, and run it with:
+# ./jekyllpostgen.sh "post title"
+
+# What is does:
+# - creates the post with the correct format of date and title
+# - adds YAML front-matter (layout, title, date, tags)
+# - opens the post file in editor chosen
+
+# EXAMPLE OUTPUT:
+
+# ---
+# layout: post
+# title: "A Canvas of the Minds"
+# href: https://acanvasoftheminds.com/
+# date: 2017-10-27 22:57:09 -0400
+# tags: [wordpress, personal_stories, collaborative, blogs]
+# categories: EN
+# ---
+
+function config {
+    # Post layout
+    layout="post"
+
+    # Post text editor
+    editor="cat"
+
+    # Post directory
+    folder="_posts/"
+
+    # Default date format in posts
+    default_date_format_in_post='%F %T %z'
+
+    # Default category (space separated values)
+    default_category="EN"
+
+    tag_format_is_brackets=true
+}
+
+function show_help {
+    echo ""
+    echo "Usage: `basename $1` \"Post title\""
+    echo ""
+    echo "For interactive mode use: `basename $1` -i"
+    echo ""
+    exit 0
+}
+
+function do_interactive_mode {
+    read -p "Enter Title: " title
+    read -p "Enter Href (link): " href
+    read -p "Enter Tags (comma separated): " tags
+    read -p "Enter Categories (space separated): " categories
+}
+
+function set_filename {
+    # convert title to jekyll post filename format 
+    # echo part replaces spaces with '-', awk converts it to lowercase
+    # sed keeps only lowercase letters and '-' and 0-9 digits
+    filetitle=$( echo ${1// /-} | awk '{print tolower($0)}'| sed 's/[^a-z0-9\-]*//g')
+
+    filename="${folder}`date +%F`-$filetitle.md"
+}
+
+config
+
+is_interactive=false
+is_basic_mode=false
+
+# show help with -h or --help, set interactive mode with -i, no params show help, no second params basic mode
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  show_help $0
+elif [[ "$1" == "-i" ]]; then
+  is_interactive=true
+elif [ -z "$1" ]; then
+  show_help $0
+elif [[ -z "$2" && "$is_interactive" = false ]]; then
+  is_basic_mode=true
+fi
+
+# if is_interactive is false an received a "-" in first param, show help
+if [[ ${1:0:1} == "-" && "$is_interactive" = false ]]; then
+    show_help $0
+fi
+
+
+##### Variables #####
+if [[ "$is_interactive" = true ]]; then
+    do_interactive_mode
+elif [[ "$is_interactive" = false && "$is_basic_mode" = false ]]; then
+    OPTIND=2
+    while getopts "l:t:c:" option; do
+        case "${option}" in
+            t) tags=${OPTARG};;
+            l) href=${OPTARG};;
+            c) categories=${OPTARG};;
+        esac
+    done
+    title="$1"
+else
+    title="$1"
+fi
+
+set_filename "$title"
+
+date=$(date +"$default_date_format_in_post")
+
+########## Adding to file ##########
+echo "---" >> $filename
+echo "layout: ${layout}" >> $filename
+echo "title: \"${title}\"" >> $filename
+
+if [[ "$is_basic_mode" = false ]]; then
+
+  ### Adding href to file
+  if [ "$href" ]; then
+    echo "href: $href" >> $filename
+  fi
+
+  ### Adding date to file
+  echo "date: $date" >> $filename
+
+  ### Adding tags
+  if [ "$tags" ]; then
+    if [[ "$tag_format_is_brackets" = true ]]; then
+        echo "tags: [$tags]" >> $filename
+    else
+    # The lack of indentation here is necessary 
+        echo "tags:
+- $tags" | sed 's/,/\
+-/g' >> $filename
+    fi
+  fi
+
+  ### Adding categories
+  if [ "$categories" ]; then
+    echo "categories: $categories" >> $filename
+  else
+    echo "categories: ${default_category}" >> $filename
+  fi
+
+fi
+
+echo "---" >> $filename
+echo >> $filename
+
+echo "Your new filename is: " $filename
+
+# open in chosen editor
+${editor} $filename
