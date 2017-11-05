5ce4922776a9be5e990ee473a103cca873f5618b
Eric Hoog <eric_hoog@hotmail.com>
2017-11-05 00:26:03 -0400

    add file name format test

--- test/tests.sh
+++ test/tests.sh
@@ -1,25 +1,29 @@
 #! /bin/bash
 
+initializeTest() {
+    cd ./test    
+    if [ ! -d tmp_test ]; then
+      mkdir tmp_test
+    fi
+    cd tmp_test
+}
+
 test_initial_check() {
   assertEquals 1 1
 }
 
-test_title_tag() {
+test_filename_format() {
   (
-    cd ./test
-    
-    if [ ! -d tmp_test ]; then
-      mkdir tmp_test
-    fi
+    initializeTest
 
-    cd tmp_test
     TITLE="Your Test SHUNIT2 Title"
     EXPECTED_FILENAME="$(date +%F)-your-test-shunit2-title.md"
 
     RESULT=$(../../jekyllpostgen.sh "$TITLE")
 
-    # NEW_RESULT=$(sed 's/^.*.\/.*.md//' <<< "$RESULT")
-    # echo $NEW_RESULT
+    if [ ! -f "$EXPECTED_FILENAME" ]; then
+      fail "ERROR: test file not found...aborting test" 
+    fi
   )
 }
 
